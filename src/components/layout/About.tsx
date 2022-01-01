2816eaa4d5affedd5c3400fc8f2011efdb9b1793
Shiho <mia.can2021@gmail.com>
2022-01-01 07:03:49 -0800

    Added hero about section

---
+++ src/components/layout/About.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from "styled-components"
+import { theme } from "../styles/Globals.styled"
+
+export const About = () => {
+    return(
+        <ThemeProvider theme={theme}>
+        
+        </ThemeProvider>
+    )
+}
