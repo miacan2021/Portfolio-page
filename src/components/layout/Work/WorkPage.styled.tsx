b8e2dc59b5fe32dc6207c33c1b1117fc6f3a17fd
Shiho <mia.can2021@gmail.com>
2022-01-11 06:25:03 -0800

    changed work page

--- src/components/layout/Work/WorkPage.styled.tsx
+++ src/components/layout/Work/WorkPage.styled.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
-export const WorkPageWrapper = styled.div`
+export const WorkPageWrapper = styled.div<{bg: string}>`
    width: 80vw;
    max-width: 1000px;
    padding-top: 130px;
    text-align: center;
    margin: auto;
+   color: ${({theme}) => theme.palette.head_text};
+   background-color: ${({bg}) => bg};
    @media (max-width: ${({theme}) => theme.mobile}) {
         width: 90wh;
        }
--- src/components/layout/Work/WorkPage.tsx
+++ src/components/layout/Work/WorkPage.tsx
@@ -3,16 +3,47 @@ import { GrobalStyles } from "../../styles/Globals.styled";
 import { NavBar } from "../pattern/NavBar"
 import { ThemeProvider } from "styled-components"
 import { theme } from "../../styles/Globals.styled"
-import Data from '../../../ProjectData.json'
+import ProjectData from '../../../ProjectData.json'
+import { WorkPageWrapper } from "./WorkPage.styled";
+import { idText } from "typescript";
 
+interface Params {
+        title: string,
+        gif: string,
+        skills: string[],
+        category: string
+    }
 
 export const WorkPage = () => {
+    const datas = ProjectData.projects
+
+    const colors :string[] = [
+       '#fffffe',
+       '#faeee7',
+       '#ff8ba7',
+       '#ffc6c7',
+       '#c3f0ca',
+    ]
+
+    const pickColor = (i: number) => {
+       if(i >= 5){
+            return colors[i-5]
+        }else{
+            return colors[i]
+        }
+    }
+
     return(
         <>
         <GrobalStyles />
         <NavBar/>
         <ThemeProvider theme={theme}>
-        {}
+        {datas.map((data: Params, i) => {
+           let color =  pickColor(i)
+            return(
+            <WorkPageWrapper key={data.title} bg={`${color}`}>{data.title}</WorkPageWrapper>
+            )
+        })}
         <Contact />
         </ThemeProvider>
         </>
