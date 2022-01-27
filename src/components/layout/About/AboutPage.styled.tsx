0ffa0427cac508363cd065abc6e88d420ef80b9d
Shiho <mia.can2021@gmail.com>
2022-01-27 00:38:37 -0800

    added animation on About page

--- src/components/layout/About/AboutPage.styled.tsx
+++ src/components/layout/About/AboutPage.styled.tsx
@@ -81,6 +81,18 @@ export const ClickText = styled.p`
     color: ${({theme}) =>  theme.palette.head_text};
     padding-bottom: 5px;
     letter-spacing: 3px;
+    animation: flash 1s linear infinite;
+    @keyframes flash {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+     }
     @media (max-width: ${({theme}) => theme.mobile}) {
         font-size: 8px;
         letter-spacing: 2px;
